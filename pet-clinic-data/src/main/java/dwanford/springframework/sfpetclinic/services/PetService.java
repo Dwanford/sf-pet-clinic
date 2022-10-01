e2a6ff6cf1f34e8f5eabe2f728cd57d373a093d8
dwanford <dwanford@hotmail.com>
2022-10-01 14:10:01 +0300

    refactoring to common crud interface. closes #10

--- pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/services/PetService.java
@@ -2,13 +2,6 @@ package dwanford.springframework.sfpetclinic.services;
 
 import dwanford.springframework.sfpetclinic.model.Pet;
 
-import java.util.Set;
+public interface PetService extends CrudService<Pet, Long> {
 
-public interface PetService {
-
-    Pet findById(Long id);
-
-    Pet save(Pet pet);
-
-    Set<Pet> findAll();
 }
