80be01e0f5c4c896e348dbf9c55cb5bb9948d574
dwanford <dwanford@hotmail.com>
2022-10-01 20:29:30 +0300

    adding owners list and updated controller. closes #15

--- pet-clinic-web/src/main/java/dwanford/springframework/sfpetclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/dwanford/springframework/sfpetclinic/bootstrap/DataLoader.java
@@ -1,5 +1,7 @@
 package dwanford.springframework.sfpetclinic.bootstrap;
 
+import dwanford.springframework.sfpetclinic.model.Owner;
+import dwanford.springframework.sfpetclinic.model.Vet;
 import dwanford.springframework.sfpetclinic.services.OwnerService;
 import dwanford.springframework.sfpetclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
@@ -19,7 +21,37 @@ public class DataLoader implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
 
+        Owner owner1 = new Owner();
+        owner1.setId(1L);
+        owner1.setFirstName("Michael");
+        owner1.setLastName("Weston");
 
+        ownerService.save(owner1);
+
+        Owner owner2 = new Owner();
+        owner2.setId(2L);
+        owner2.setFirstName("Fiona");
+        owner2.setLastName("Glenanne");
+
+        ownerService.save(owner2);
+
+        System.out.println("Loaded Owners....");
+
+        Vet vet1 = new Vet();
+        vet1.setId(1L);
+        vet1.setFirstName("Sam");
+        vet1.setLastName("Axe");
+
+        vetService.save(vet1);
+
+        Vet vet2 = new Vet();
+        vet2.setId(2L);
+        vet2.setFirstName("Jessie");
+        vet2.setLastName("Porter");
+
+        vetService.save(vet1);
+
+        System.out.println("Loaded Vets....");
 
     }
 }
