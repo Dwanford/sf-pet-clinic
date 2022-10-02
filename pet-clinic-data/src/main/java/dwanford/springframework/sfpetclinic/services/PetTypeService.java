382e4f845b94311e42aa07cb28ab45039aab2e4c
dwanford <dwanford@hotmail.com>
2022-10-02 17:00:11 +0300

    added PetTypeService, closes #28

---
+++ pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/services/PetTypeService.java
@@ -0,0 +1,6 @@
+package dwanford.springframework.sfpetclinic.services;
+
+import dwanford.springframework.sfpetclinic.model.PetType;
+
+public interface PetTypeService extends CrudService<PetType, Long>{
+}
