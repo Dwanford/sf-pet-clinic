e2a67ac685330c861e0ace9b8603de3e1b4f6ef1
dwanford <dwanford@hotmail.com>
2022-10-02 17:40:59 +0300

    added specialities map impl. closes #30

---
+++ pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/services/SpecialitiesService.java
@@ -0,0 +1,6 @@
+package dwanford.springframework.sfpetclinic.services;
+
+import dwanford.springframework.sfpetclinic.model.Speciality;
+
+public interface SpecialitiesService extends CrudService<Speciality, Long> {
+}
