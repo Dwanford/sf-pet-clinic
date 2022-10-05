45ac34055b9ac345a240990b9e82baee6364a91f
dwanford <dwanford@hotmail.com>
2022-10-05 21:07:50 +0300

    added spring data jpa for owners. clsoses #39

--- pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/repositories/OwnerRepository.java
@@ -4,4 +4,7 @@ import dwanford.springframework.sfpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
+
+    Owner findByLastName(String lastName);
+
 }
