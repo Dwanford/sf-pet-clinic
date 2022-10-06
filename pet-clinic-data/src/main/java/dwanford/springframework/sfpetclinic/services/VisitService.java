026b256d7e2e2d2f0afaf49f70fd58e41480c26a
dwanford <dwanford@hotmail.com>
2022-10-06 12:45:58 +0300

    implemented map impl for visit service. closes #45

---
+++ pet-clinic-data/src/main/java/dwanford/springframework/sfpetclinic/services/VisitService.java
@@ -0,0 +1,6 @@
+package dwanford.springframework.sfpetclinic.services;
+
+import dwanford.springframework.sfpetclinic.model.Visit;
+
+public interface VisitService extends CrudService<Visit, Long> {
+}
