a891af8810f5f0a804dd7503a3d586efc49737e3
dwanford <dwanford@hotmail.com>
2022-10-18 19:00:23 +0300

    display owner record. closes #52

--- pet-clinic-web/src/main/java/com/dwanford/sfpetclinic/controllers/OwnerController.java
+++ pet-clinic-web/src/main/java/com/dwanford/sfpetclinic/controllers/OwnerController.java
@@ -3,7 +3,10 @@ package com.dwanford.sfpetclinic.controllers;
 import com.dwanford.sfpetclinic.services.OwnerService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 @RequestMapping("/owners")
 @Controller
@@ -25,5 +28,12 @@ public class OwnerController {
     @RequestMapping("/find")
     public String findOwners(){
         return "notimplemented";
+    }
+
+    @GetMapping("/{ownerId}")
+    public ModelAndView showOwner(@PathVariable("ownerId") Long id) {
+        ModelAndView mav = new ModelAndView("owners/ownerDetails");
+        mav.addObject(ownerService.findById(id));
+        return mav;
     }
 }
