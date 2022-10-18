0c302304f9477ce248484a3bca483889d329b903
dwanford <dwanford@hotmail.com>
2022-10-18 21:58:42 +0300

    implemented find owner by last name. closes #53

--- pet-clinic-data/src/main/java/com/dwanford/sfpetclinic/repositories/OwnerRepository.java
+++ pet-clinic-data/src/main/java/com/dwanford/sfpetclinic/repositories/OwnerRepository.java
@@ -3,8 +3,12 @@ package com.dwanford.sfpetclinic.repositories;
 import com.dwanford.sfpetclinic.model.Owner;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+
 public interface OwnerRepository extends CrudRepository<Owner, Long> {
 
     Owner findByLastName(String lastName);
+
+    List<Owner> findAllByLastNameLike(String lastName);
 
 }
--- pet-clinic-data/src/main/java/com/dwanford/sfpetclinic/services/OwnerService.java
+++ pet-clinic-data/src/main/java/com/dwanford/sfpetclinic/services/OwnerService.java
@@ -2,8 +2,10 @@ package com.dwanford.sfpetclinic.services;
 
 import com.dwanford.sfpetclinic.model.Owner;
 
+import java.util.List;
+
 public interface OwnerService extends CrudService<Owner, Long> {
 
     Owner findByLastName(String lastName);
-
+    List<Owner> findAllByLastNameLike(String lastName);
 }
